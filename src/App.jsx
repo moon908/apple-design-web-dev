fea48ae58949bb84100fcc7781d5a210b42d8142
Siddhesh Moon <siddheshmoon908u@gmail.com>
2025-12-31 20:07:48 +0530

    Implement Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+
+export default App
